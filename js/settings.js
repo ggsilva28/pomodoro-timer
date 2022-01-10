5c24703481a562921bfb7a194e301acbf1bfeb4c
ggsilva <gagsilva28@gmail.com>
2022-01-10 17:13:56 -0300

    Correções no cronômetro | Melhoria visual na porcentagem do tempo

--- js/settings.js
+++ js/settings.js
@@ -6,20 +6,54 @@ settings.init = () => {
 
 settings.timer = {}
 
-settings.timer.pomodoro = {
-    minutes: 25,
-    seconds: 0
-}
+settings.timer.pomodoro = 1500
 
-settings.timer.short_break = {
-    minutes: 5,
-    seconds: 0
-}
+settings.timer.short_break = 300
 
-settings.timer.long_break = {
-    minutes: 15,
-    seconds: 0
-}
+settings.timer.long_break = 900
+
+settings.colors = [
+    {
+        primary: '#c31432',
+        primary_rgb: '195, 20, 50',
+        secondary: '#2e1541',
+        secondary_rgb: '36, 11, 54',
+        secondary_fade: '#2e1541',
+        text: '#ffffff'
+    },
+    {
+        primary: '#F27121',
+        primary_rgb: '',
+        secondary: '#8A2387',
+        secondary_rgb: '',
+        secondary_fade: '',
+        text: '#ffffff'
+    },
+    {
+        primary: '#EB5757',
+        primary_rgb: '',
+        secondary: '#111111',
+        secondary_rgb: '',
+        secondary_fade: '',
+        text: '#ffffff'
+    },
+    {
+        primary: '#66ff00',
+        primary_rgb: '',
+        secondary: '#a80077',
+        secondary_rgb: '',
+        secondary_fade: '',
+        text: '#ffffff'
+    },
+    {
+        primary: '#0b8793',
+        primary_rgb: '',
+        secondary: '#360033',
+        secondary_rgb: '',
+        secondary_fade: '',
+        text: '#ffffff'
+    }
+]
 
 settings.getUserSetting = () => {
 
@@ -31,6 +65,5 @@ settings.getUserSetting = () => {
         }
     }
 }
-
 
 export default settings
--- js/timer.js
+++ js/timer.js
@@ -2,6 +2,8 @@ import settings from "./settings.js"
 
 const timer = {}
 
+timer.interval = null;
+
 timer.elMinutes = document.getElementById('timer-minutes')
 timer.elSeconds = document.getElementById('timer-seconds')
 
@@ -16,7 +18,7 @@ timer.init = () => {
     timer.listeners()
 }
 
-timer.listeners = () => {
+timer.listeners = async () => {
     const container = document.getElementById('timer-container')
     const controller = container.querySelector('#timer-controller')
 
@@ -24,43 +26,52 @@ timer.listeners = () => {
         let action = controller.getAttribute('action')
 
         if (action == 'restart') {
-            timer.percentage(0)
             timer.set(timer.setting)
+            timer.resetPercentage()
         }
 
-        if (action == 'start' || action == 'restart' || action == 'continue') {
-            controller.setAttribute('action', 'pause')
-            controller.innerHTML = 'PAUSE'
-            document.body.classList.add('run')
-            timer.countDown()
-        }
+        setTimeout(() => {
+            if (action == 'start' || action == 'restart' || action == 'continue') {
+                controller.setAttribute('action', 'pause')
+                controller.innerHTML = 'PAUSE'
+                document.body.classList.add('run')
+                timer.percentage()
+                if (!timer.interval) {
+                    timer.interval = setInterval(timer.countDown, 100);
+                }
+            }
 
-        if (action == 'pause') {
-            controller.setAttribute('action', 'continue')
-            controller.innerHTML = 'CONTINUE'
-            timer.stop()
-        }
-
+            if (action == 'pause') {
+                controller.setAttribute('action', 'continue')
+                controller.innerHTML = 'CONTINUE'
+                timer.stop()
+            }
+        }, 10)
     })
 
     const types = document.getElementById('timer-types')
     types.querySelectorAll('li').forEach(el => {
-
         el.addEventListener('click', () => {
+            container.classList.add('await')
+
             const type = el.getAttribute('type')
             types.querySelectorAll('li').forEach(allEl => {
                 allEl.classList.remove('active')
-
             })
 
             el.classList.add('active')
             controller.setAttribute('action', 'start')
             controller.innerHTML = 'START'
-            
+
             timer.setting = type
             timer.stop()
-            timer.percentage(0)
             timer.set(type)
+
+            timer.resetPercentage()
+
+            setTimeout(() => {
+                container.classList.remove('await')
+            }, 300)
         })
     })
 }
@@ -72,73 +83,104 @@ timer.allowReset = () => {
 }
 
 timer.countDown = () => {
-    let minutes = timer.minutes
-    let seconds = timer.seconds
-
-    if (minutes == 0 && seconds == 0) {
+    if (timer.seconds == 0) {
         timer.allowReset()
         timer.stop()
     }
 
-    if (seconds == 0) {
-        seconds = 59
-        minutes = minutes - 1
-    } else {
-        seconds = seconds - 1
+    timer.seconds = timer.seconds - 0.1
+    if (document.body.classList.contains('run')) {
+        timer.update()
+    }else{
+        timer.stop()
     }
-
-    setTimeout(() => {
-        if (document.body.classList.contains('run')) {
-            timer.update(minutes, seconds)
-            timer.percentage()
-            timer.countDown()
-        }
-    }, 1000)
 }
 
 timer.stop = () => {
+    timer.pausePercentage()
     document.body.classList.remove('run')
+    if (timer.interval) {
+        clearInterval(timer.interval);
+        timer.interval = null;
+    }
 }
 
 timer.set = (setting) => {
-    const minutes = settings.timer[setting].minutes;
-    const seconds = settings.timer[setting].seconds;
-    timer.update(minutes, seconds)
+    timer.seconds = settings.timer[setting]
+    console.log(timer.seconds)
+    timer.update()
 }
 
-timer.update = (minutes, seconds) => {
-    console.log(minutes, ':', seconds)
-    timer.minutes = minutes;
-    timer.seconds = seconds;
-    timer.elMinutes.innerHTML = timer.formatter(minutes)
-    timer.elSeconds.innerHTML = timer.formatter(seconds)
+timer.update = () => {
+    const time = timer.formatter(timer.seconds)
+    timer.elMinutes.innerHTML = time[0]
+    timer.elSeconds.innerHTML = time[1]
 }
 
-timer.formatter = (time) => {
+timer.formatter = (duration) => {
+    // minutes and seconds
+    var mins = ~~((duration % 3600) / 60);
+    var secs = ~~duration % 60;
 
-    if (time == 0) {
-        return '00'
+    // Output like "1:01" or "4:03:59" or "123:03:59"
+    var ret = "";
+
+    ret += "" + mins + ":" + (secs < 10 ? "0" : "");
+    ret += "" + secs;
+    return ret.split(':')
+}
+
+
+timer.percentage = async (value = null) => {
+    const el = document.getElementById('timer-percentage')
+    const calc = timer.calculatePercentage()
+
+    el.style.transition = 'all 0ms linear 0s'
+    if (calc.percentage == 100) {
+        timer.setCirclePercentage(0)
+    } else {
+        timer.setCirclePercentage(calc.percentage)
     }
 
-    if (time > 0 && time < 10) {
-        return '0' + String(time)
-    }
-
-    return time
+    el.style.transition = 'all ' + String((calc.current_time * 1000) - 1000) + 'ms linear 0s'
+    timer.setCirclePercentage(100)
 }
 
-timer.percentage = (value = null) => {
+timer.resetPercentage = () => {
+    const el = document.getElementById('timer-percentage')
+
+    el.style.transition = 'all 0ms linear 0s'
+    timer.setCirclePercentage(0)
+}
+
+timer.pausePercentage = () => {
+    const el = document.getElementById('timer-percentage')
+    const calc = timer.calculatePercentage()
+
+    el.style.transition = 'all 0ms linear 0s'
+    timer.setCirclePercentage(calc.percentage)
+}
+
+timer.setCirclePercentage = (percentage) => {
     const root = document.querySelector(':root');
-
-    if(value != null){
-        return root.style.setProperty('--circle-percentage', value);
-    }
-
-    const total_time = Number(settings.timer[timer.setting].minutes) * 60 + Number(settings.timer[timer.setting].seconds)
-    const current_time = Number(timer.minutes) * 60 + Number(timer.seconds)
-    const percentage = 100 - ((100 * current_time) / total_time) 
-
     root.style.setProperty('--circle-percentage', percentage);
 }
- 
+
+timer.calculatePercentage = () => {
+    const total_time = Number(settings.timer[timer.setting])
+    const current_time = Number(timer.seconds)
+    const percentage = 100 - (100 * current_time) / total_time
+
+    console.log({
+        total_time: total_time,
+        current_time: current_time,
+        percentage: percentage
+    })
+    return {
+        total_time: total_time,
+        current_time: current_time,
+        percentage: percentage
+    }
+}
+
 timer.init()
